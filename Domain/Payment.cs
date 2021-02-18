16bbbe24bf7da046084db577a175cf3d82460df4
UCHE IGBOKWE <uchehenryigbokwe@gmail.com>
2021-02-18 09:42:13 +0100

    setup database migration

---
+++ Domain/Payment.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Domain
+{
+    public class Payment
+    {
+        public Guid Id { get; set; }
+        public string CreditCardNumber { get; set; }
+        public string CardHolder { get; set; }
+        public DateTime ExpirationDate { get; set; }
+        public string SecurityCode { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
---
+++ Persistence/Migrations/20210218082803_InitialCreate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Payments",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    CreditCardNumber = table.Column<string>(type: "TEXT", nullable: true),
+                    CardHolder = table.Column<string>(type: "TEXT", nullable: true),
+                    ExpirationDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    SecurityCode = table.Column<string>(type: "TEXT", nullable: true),
+                    Amount = table.Column<decimal>(type: "TEXT", nullable: false)
+                },
+                constraints: table => table.PrimaryKey("PK_Payments", x => x.Id));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Payments");
+        }
+    }
+}
